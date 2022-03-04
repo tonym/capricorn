c23575df575ea23fea616ef6a197964e8b71ffbf
Tony M <tonym@Tonys-iMac.local>
2022-03-04 12:13:24 -0800

    css baseline

--- apps/og-mui/src/app/app.tsx
+++ apps/og-mui/src/app/app.tsx
@@ -1,13 +1,62 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import {
+  ButtonStyles,
+  cssBaseline,
+  useTheme as OgUseTheme,
+} from '@opensesame/gemini';
+import { AppBar, Button } from '@mui/material';
 
-export function App() {
+const ogTheme = OgUseTheme() as any;
+
+const buttonStyles = ButtonStyles(OgUseTheme()) as any;
+
+delete buttonStyles.root.backgroundColor;
+delete buttonStyles.root.color;
+delete buttonStyles.root['&:hover'];
+
+ogTheme.components = {
+  MuiButton: {
+    styleOverrides: {
+      root: buttonStyles.root,
+    },
+  },
+};
+
+delete ogTheme.spacing;
+
+const theme = createTheme(ogTheme);
+
+const AppBarPrimary = styled(AppBar)({
+  padding: theme.spacing(3),
+});
+
+const AppWrapper = styled('div')({
+  paddingLeft: theme.spacing(3),
+  paddingRight: theme.spacing(3),
+  paddingTop: theme.spacing(10),
+});
+
+cssBaseline();
+
+function App() {
   return (
-    <>
-      <NxWelcome title="og-mui" />
-      <div />
-    </>
+    <ThemeProvider theme={theme}>
+      <AppWrapper>
+        <AppBarPrimary>Gemini for React</AppBarPrimary>
+        <div>
+          <Button variant="contained">Primary contained</Button>
+          <Button variant="outlined">Primary outlined</Button>
+        </div>
+        <div>
+          <Button color="secondary" variant="contained">
+            Secondary contained
+          </Button>
+          <Button color="secondary" variant="outlined">
+            Secondary outlined
+          </Button>
+        </div>
+      </AppWrapper>
+    </ThemeProvider>
   );
 }
 
