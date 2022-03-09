97d7af479a764b00d49d6402339894b4bcce9348
Tony M <tony@fbpllc.com>
2022-03-09 11:02:31 -0800

    update styles

--- apps/og-radix/src/app/app.tsx
+++ apps/og-radix/src/app/app.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { cssBaseline, useTheme as ogTheme } from '@opensesame/gemini';
+import { cssBaseline, useTheme } from '@opensesame/gemini';
 import { styled, keyframes } from '@stitches/react';
-import { violet, mauve, blackA } from '@radix-ui/colors';
+import { blackA } from '@radix-ui/colors';
 import { MixerHorizontalIcon, Cross2Icon } from '@radix-ui/react-icons';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 
-const theme = ogTheme();
+const theme = useTheme();
 cssBaseline();
 
 const slideUpAndFade = keyframes({
@@ -28,12 +28,11 @@ const slideLeftAndFade = keyframes({
   '100%': { opacity: 1, transform: 'translateX(0)' },
 });
 const StyledContent = styled(PopoverPrimitive.Content, {
-  borderRadius: 4,
-  padding: 20,
+  borderRadius: theme.spacing.unit / 2,
+  padding: theme.spacing.unit * 2,
   width: 260,
   backgroundColor: theme.palette.background.default,
-  boxShadow:
-    'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
+  boxShadow: theme.shadows[3],
   '@media (prefers-reduced-motion: no-preference)': {
     animationDuration: '400ms',
     animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
@@ -47,7 +46,7 @@ const StyledContent = styled(PopoverPrimitive.Content, {
     },
   },
   '&:focus': {
-    boxShadow: `hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px, 0 0 0 2px ${violet.violet7}`,
+    boxShadow: theme.shadows[3],
   },
 });
 
@@ -57,20 +56,18 @@ const StyledArrow = styled(PopoverPrimitive.Arrow, {
 
 const StyledClose = styled(PopoverPrimitive.Close, {
   all: 'unset',
-  fontFamily: theme.typography.variants.body1?.fontFamily,
   borderRadius: '100%',
   height: 25,
   width: 25,
   display: 'inline-flex',
   alignItems: 'center',
   justifyContent: 'center',
-  color: theme.palette.grey[900],
   position: 'absolute',
   top: 5,
   right: 5,
 
-  '&:hover': { backgroundColor: violet.violet4 },
-  '&:focus': { boxShadow: `0 0 0 2px ${violet.violet7}` },
+  '&:hover': { backgroundColor: theme.palette.primary.light },
+  '&:focus': { boxShadow: theme.shadows[1] },
 });
 
 // Exports
@@ -92,11 +89,11 @@ const IconButton = styled('button', {
   display: 'inline-flex',
   alignItems: 'center',
   justifyContent: 'center',
-  color: violet.violet11,
+  color: theme.palette.primary.main,
   backgroundColor: 'white',
-  boxShadow: `0 2px 10px ${blackA.blackA7}`,
-  '&:hover': { backgroundColor: violet.violet3 },
-  '&:focus': { boxShadow: `0 0 0 2px black` },
+  boxShadow: theme.shadows[3],
+  '&:hover': { backgroundColor: theme.palette.primary.light },
+  '&:focus': { boxShadow: theme.shadows[1] },
 });
 const Fieldset = styled('fieldset', {
   all: 'unset',
@@ -107,7 +104,6 @@ const Fieldset = styled('fieldset', {
 
 const Label = styled('label', {
   fontSize: 13,
-  color: violet.violet11,
   width: 75,
 });
 
@@ -118,30 +114,20 @@ const Input = styled('input', {
   alignItems: 'center',
   justifyContent: 'center',
   flex: '1',
-  borderRadius: 4,
-  padding: '0 10px',
-  fontSize: 13,
+  borderRadius: theme.spacing.unit / 4,
+  padding: '0 8px',
+  fontSize: 14,
   lineHeight: 1,
-  color: violet.violet11,
-  boxShadow: `0 0 0 1px ${violet.violet7}`,
   height: 25,
-
-  '&:focus': { boxShadow: `0 0 0 2px ${violet.violet8}` },
+  border: `1px solid ${theme.palette.secondary.light}`,
+  '&:focus': {
+    border: `1px solid ${theme.palette.primary.main}`,
+  },
 });
 
 const Text = styled('div', {
+  ...theme.typography.variants.body1,
   margin: 0,
-  color: mauve.mauve12,
-  fontSize: 15,
-  lineHeight: '19px',
-  variants: {
-    faded: {
-      true: { color: mauve.mauve10 },
-    },
-    bold: {
-      true: { fontWeight: 500 },
-    },
-  },
 });
 
 const App = () => (
@@ -153,9 +139,7 @@ const App = () => (
     </PopoverTrigger>
     <PopoverContent sideOffset={5}>
       <Flex css={{ flexDirection: 'column', gap: 10 }}>
-        <Text bold css={{ marginBottom: 10 }}>
-          Dimensions
-        </Text>
+        <Text css={{ marginBottom: 10 }}>Dimensions</Text>
         <Fieldset>
           <Label htmlFor="width">Width</Label>
           <Input id="width" defaultValue="100%" />
